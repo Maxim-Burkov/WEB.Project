8bbe0157d1a5373a6b1ab7e5b647cc6677cabfd5
MMM <sup3r44ax@yandex.ru>
2021-03-27 17:33:59 +0300

    guide

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import Flask
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return 'Hello World'
 
 if __name__ == "__main__":
     app.run(debug=True)
