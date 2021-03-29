42ca0cab5da0bf5ab5367dbbd4f8ea319aeafdf9
MMM <sup3r44ax@yandex.ru>
2021-03-29 10:12:40 +0300

    guide
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+app = Flask(__name__)
+
 @app.route('/')
 def index():
-    return 'Hello World'
+    return 'Hello Flask'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
